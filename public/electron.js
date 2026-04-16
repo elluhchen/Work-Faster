6538cf74021bb03f858d2699c0a858dd4710b1b8
elluhchen <ellamadelynchen09@icloud.com>
2026-04-16 18:36:52 -0500

    Add files via upload

---
+++ public/electron.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require("electron");
+const { ipcMain } = require("electron");
+const url = require("url");
+const path = require("path");
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "WorkFaster",
+    width: 400,
+    height: 400,
+    frame: false,
+    titleBarStyle: "hidden",
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"), // Path to preload script
+      contextIsolation: true, // Keeps context isolated for security
+      nodeIntegration: false, // Disables Node.js in the renderer (security best practice)
+    },
+  });
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, "../build/index.html"), //connect to the react app
+    protocol: "file",
+    slashes: true,
+  });
+
+  mainWindow.loadURL(startUrl); //load app in electron window
+
+  // listen for 'close-app' event
+  ipcMain.on("close-app", () => {
+    app.quit();
+  });
+}
+
+app.whenReady().then(createMainWindow);
